3499f6a058fbe983cf2db8d4bf7eda3e5524a67d
Amit Ranan <ranan.amit@gmail.com>
2015-07-30 16:56:56 -0700

    hadouken

--- js/app.js
+++ js/app.js
@@ -1,41 +1,41 @@
 $( document ).ready(function() {
-    $(".img2").hide();
-    $(".img3").hide();
-    $(".img4").hide();
 
-    $(".img1").mouseenter(function() {
-    	$(".img1").hide();
-    	$(".img2").show();
-    });
+  $(".ryu").mouseenter(function() {
+    $(".ryu-still").hide();
+    $(".ryu-ready").show();
+  })
 
-    $(".img2").mouseleave(function() {
-    	$(".img2").hide();
-    	$(".img1").show();
-    });
+  .mouseleave(function() {
+    $(".ryu-still").show();
+    $(".ryu-ready").hide();
+  })
 
-    $(".img2").mousedown(function() {
-    	$(".img2").hide();
-    	$(".img3").show();
-        $(".img4").show();
-    });
+  .mousedown(function() {
+    playHadouken();
+    $(".ryu-still").hide();
+    $(".ryu-ready").hide();
+    $(".ryu-throwing").show();
+    $('.hadouken').finish().show().animate(
+      {'left': '1020px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '520px');
+      }
+    );
 
-    $(".img3").mouseup(function() {
-    	$(".img3").hide();
-        $(".img4").hide();
-    	$(".img2").show();
-    });
+  })
 
-    
+  .mouseup(function() {
+    $(".ryu-still").show();
+    $(".ryu-throwing").hide();
+  });
 
-
-
-
-
-
-
-
-
-
+  function playHadouken () {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+  }
 
 
 
